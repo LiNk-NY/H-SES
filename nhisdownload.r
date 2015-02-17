12f02346835b378531d497d7beb4fd6c14ab7826
LiNk-NY <mramos09@gmail.com>
2015-02-17 08:42:48 -0500

    working on loading file

--- nhisdownload.r
+++ nhisdownload.r
@@ -4,6 +4,7 @@
 library(RCurl)
 library(SAScii)
 library(foreign)
+library(data.table)
 
 # Disable use of Internet Explorer for internet access
 setInternet2(use=FALSE)
@@ -16,7 +17,7 @@ getNHISdata <- function(begin = 1986, finish = 2004, todir = NULL, docs = FALSE)
                 }
         linkedFTP <- "ftp://ftp.cdc.gov/pub/Health_Statistics/NCHS/datalinkage/linked_mortality/"
         listFiles <- getURL(linkedFTP, ftp.use.epsv = FALSE, dirlistonly = TRUE)
-        datFiles <- strsplit(listFiles, "\r*\n")
+        listFiles <- unlist(strsplit(listFiles, "\r*\n"))
         datFiles <- tolower(unlist(lapply(datFiles, function(x){grep("NHIS", x, value=TRUE)})))
         #removing unmodified 1992 file due to oversampling of Hispanics in 1992 - MOD file is appropriate
         datFiles <- datFiles[-grep("nhis92_mort", datFiles)]
@@ -25,7 +26,7 @@ getNHISdata <- function(begin = 1986, finish = 2004, todir = NULL, docs = FALSE)
         if(!is.null(todir)){
                 if(!class(todir)=="character"){
                         stop("Please enter a valid directory path!")
-                } else if(!file.exists(todir)){
+                } else if(!file.exists(file.path(todir))){
                         cat("Directory does not exist >> creating one...")
                         dir.create(todir, recursive=TRUE)
                 }
@@ -38,7 +39,7 @@ getNHISdata <- function(begin = 1986, finish = 2004, todir = NULL, docs = FALSE)
         # download documentation if TRUE
         if(docs){
                 download.file(paste0(linkedFTP,"nhis_readme_mortality_2010.txt"), 
-                        destfile = file.path(todir, "nhis_readme_mortality_2010.txt"), method="auto", quiet = FALSE)
+                        destfile = file.path(todir, "nhis_readme_mortality_2010.txt"), method="auto", mode="wb", quiet = FALSE)
         }
         # simple check for valid years
         if(identical(begin, finish)){
@@ -52,3 +53,14 @@ getNHISdata <- function(begin = 1986, finish = 2004, todir = NULL, docs = FALSE)
                 }
                }
 }
+
+procNHIS <- function(fromdir= todir, ){
+        sasFile <- tolower(listFiles[grep("nhis_sample_ascii", listFiles)])
+        sasciistart <- grep("INPUT ALL VARIABLES", readLInes(paste0(linkedFTP, sasFile))) + 1
+        
+        read.SAScii(file.path(todir, "nhis00_mort_public_use_2010.dat"),paste0(linkedFTP, sasFile), beginline = sasciistart )
+}
+
+
+
+
